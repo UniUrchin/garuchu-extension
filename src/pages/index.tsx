80c5b38379f37497e2444cf0d0b074cbfbd39f3d
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-03-28 18:41:57 +0900

    :lipstick: postCSSを使って、見づらいスタイルをまとめた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,22 @@
 import { NextPage } from "next";
 import { postMessage } from "../api/postMessage";
+import styles from "../styles/Index.module.css";
 
 const IndexPage: NextPage = () => {
   return (
-    <div className="flex justify-center">
-      <div className="w-80 h-80 flex flex-col justify-center items-center">
-        <h1 className="font-semibold mx-4 my-2">
-          つよつよポップアップ建設予定地!!
-        </h1>
-        <div
-          className="bg-blue-500 hover:bg-blue-700 font-semibold text-white px-4 py-2 rounded hover:cursor-pointer duration-100"
-          onClick={postMessage}
-        >
+    <div className={styles.popupContainer}>
+      <div className={styles.header}>
+        <img src="icons/icon128.png" width="30rem" height="30rem"></img>
+        <span className="w-2"></span>
+        <h1 className="text-2xl">Garuchu Extension</h1>
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="font-semibold my-4">つよつよポップアップ建設予定地!!</h1>
+        <div className={styles.postButton} onClick={postMessage}>
           Discordに投稿
         </div>
       </div>
+      <div className={styles.footer} />
     </div>
   );
 };
